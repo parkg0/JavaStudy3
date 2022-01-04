4fcc9872064adaa2e314371db4b71df961f6aaf1
jiyoung <wldud9883@naver.com>
2022-01-04 17:33:08 +0900

    update

--- src/com/google/s1/student/StudentMain.java
+++ src/com/google/s1/student/StudentMain.java
@@ -7,9 +7,8 @@ public class StudentMain {
 		//이름 번호 국어 영어 수학 출력
 StudentUtil su=new StudentUtil();
 
-Student student=new Student();
+Student student=su.makeStudent();
 
-student=su.makeStudent();
 System.out.println("이름:"+student.name);
 System.out.println("번호:"+student.number);
 System.out.println("국어점수:"+student.kor);
@@ -17,6 +16,20 @@ System.out.println("영어점수:"+student.eng);
 System.out.println("수학점수:"+student.math);
 
 
+
+Student[] students;
+students =su.makeStudents();
+
+for(int i=0;i<students.length;i++) {
+	System.out.println(i+"번째 학생의 정보");
+System.out.println("이름:"+students[i].name);
+System.out.println("번호:"+students[i].number);
+System.out.println("국어:"+students[i].kor);
+System.out.println("영어:"+students[i].eng);
+System.out.println("수학:"+students[i].math);
+
+
+}
 	}
 
 }
--- src/com/google/s1/student/StudentUtil.java
+++ src/com/google/s1/student/StudentUtil.java
@@ -3,24 +3,52 @@ package com.google.s1.student;
 import java.util.Scanner;
 
 public class StudentUtil {
+
+	public  Student[] makeStudents() {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("학생의 수를 입력하세요.");
+		int num= sc.nextInt();
+		Student[] students=new Student[num];
+		
+		for(int i=0;i<students.length;i++) {
+			Student stu=new Student();
+			students[i]=stu;
+			System.out.println("이름을 입력하세요");
+			stu.name=sc.next();
+			System.out.println("번호를 입력하세요");
+			stu.number = sc.nextInt();
+			System.out.println("국어를 입력하세요");
+			stu.kor = sc.nextInt();
+			System.out.println("영어를 입력하세요");
+			stu.eng = sc.nextInt();
+			System.out.println("수학 입력하세요");
+			stu.math = sc.nextInt();
+			
+			
+		}
+		return students;
+		// 학생의 수를 입력받아
+		//// 키보드로부터 학생 모두 이름,번호,국어, 영어 수학 입력 받아
+		// 학생들의 정보를 리턴.
+	}
+
 	public Student makeStudent() {
-		//키보드로부터 이름,번호,국어, 영어 수학 입력 리
-Scanner sc=new Scanner(System.in);
-Student stu= new Student();
+		// 키보드로부터 이름,번호,국어, 영어 수학 입력 리
+		Scanner sc = new Scanner(System.in);
+		Student stu = new Student();
 
-System.out.println("이름을 입력하세요");
-stu.name=sc.next();
-System.out.println("번호를 입력하세요");
-stu.number=sc.nextInt();
-System.out.println("국어를 입력하세요");
-stu.kor=sc.nextInt();
-System.out.println("영어를 입력하세요");
-stu.eng=sc.nextInt();
-System.out.println("수학 입력하세요");
-stu.math=sc.nextInt();
+		System.out.println("이름을 입력하세요");
+		stu.name = sc.next();
+		System.out.println("번호를 입력하세요");
+		stu.number = sc.nextInt();
+		System.out.println("국어를 입력하세요");
+		stu.kor = sc.nextInt();
+		System.out.println("영어를 입력하세요");
+		stu.eng = sc.nextInt();
+		System.out.println("수학 입력하세요");
+		stu.math = sc.nextInt();
 
-return stu;
-
+		return stu;
 
 	}
 

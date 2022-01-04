f4ecce5c4537a018636cba5d20a46fcfd6e663be
jiyoung <wldud9883@naver.com>
2022-01-04 14:34:56 +0900

    make

---
+++ src/com/google/s1/method/MethodMain.java
@@ -0,0 +1,11 @@
+package com.google.s1.method;
+
+public class MethodMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("MainMethod Start");
+System.out.println("MainMethod Finish");
+	}
+
+}

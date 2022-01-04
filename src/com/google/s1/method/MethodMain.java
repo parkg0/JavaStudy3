20cee386836d0ad59c0bfc325bbc6b0ebddd2aeb
jiyoung <wldud9883@naver.com>
2022-01-04 15:34:42 +0900

    study

--- src/com/google/s1/method/MethodMain.java
+++ src/com/google/s1/method/MethodMain.java
@@ -5,6 +5,30 @@ public class MethodMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("MainMethod Start");
+//-----------------------------
+
+
+//객체 생성코드
+//new 클래스명();
+
+ReturnStudy rs= new ReturnStudy();
+
+//멤버메서드 접근
+//참조변수명.멤버들
+
+rs.test1();
+
+int count=rs.test2();
+//random한 숫자를 여기다 출력하고싶어 
+System.out.println("main:"+count);
+
+
+String name =rs.test3();
+System.out.println("test3:"+name);
+
+double a=Math.random();
+System.out.println(a);
+//-------------------
 System.out.println("MainMethod Finish");
 	}
 
---
+++ src/com/google/s1/method/ReturnStudy.java
@@ -0,0 +1,35 @@
+package com.google.s1.method;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ReturnStudy {
+	// 멤버메서드 선언
+	// 접근지정자 [그외지정자] 리턴타입 메서드명([매개변수선언]){}
+	public void test1() {
+		System.out.println("void는 return이 없을 때 사용");
+	}
+
+	public String test3() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("이름을 입력하세요");
+		String name = sc.next(); 
+		//				호출
+		
+		return name;
+	}
+	
+	
+	
+	public int test2() {
+		Random random = new Random();
+		// class Random type 변수 random
+		// 랜덤클래스의 객체 만들기
+		int num=random.nextInt();
+		//num을 메인에 출력하고싶다 =호출한 곳으로 되돌려주고싶다. 
+		System.out.println("test2:"+num);
+		return num;
+		
+		
+	}
+}
